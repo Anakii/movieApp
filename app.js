2fda3db65b815c7aa41e2717448f342949fec5bf
or anaki <oranaki9@yahoo.com>
2019-12-08 20:04:05 +0200

    wip

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use(express.static(__dirname + "/dist/mdb-angular-free"));
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/mdb-angular-free/index.html"));
 });
